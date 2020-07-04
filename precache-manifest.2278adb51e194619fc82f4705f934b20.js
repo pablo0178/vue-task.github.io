afde33a38dd7248f538162043623def8b8e6d1ef
pablo0178 <shestoe6@gmail.com>
2020-07-04 22:06:37 +0300

    Deploy

---
+++ precache-manifest.2278adb51e194619fc82f4705f934b20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641bed36e01c7e917ade",
+    "url": "/css/app.9c164754.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3cba03f2b2d8a22464fad06f94f1ea7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5566b6e7a15f221a0e",
+    "url": "/js/about.47a44355.js"
+  },
+  {
+    "revision": "641bed36e01c7e917ade",
+    "url": "/js/app.33375553.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "/js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "1e74f20b78ad43d51554d312e7321623",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
